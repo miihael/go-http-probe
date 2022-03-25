59db079496b82d01bf50e528d6b634762a45e167
Mikhail Anisimov <manisimov@nvidia.com>
2022-03-25 17:23:53 +0300

    add SelectAll method returning many URLs and elapsed times

--- http-probe.go
+++ http-probe.go
@@ -10,6 +10,12 @@ import (
 	"golang.org/x/net/context/ctxhttp"
 )
 
+type Probe struct {
+	Url     string
+	Elapsed time.Duration
+}
+
+//Select the fastest URL responded OK within timeout
 func Select(urls []string, timeout time.Duration, client *http.Client) (string, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -42,10 +48,53 @@ func Select(urls []string, timeout time.Duration, client *http.Client) (string, 
 	}
 }
 
+//SelectURLs returns the fastest URL from URL objects responded OK within timeout
 func SelectURLs(urls []url.URL, timeout time.Duration, client *http.Client) (string, error) {
 	strs := make([]string, len(urls))
 	for i, u := range urls {
 		strs[i] = u.String()
 	}
 	return Select(strs, timeout, client)
+}
+
+//SelectAll returns URLs and elapsed times in order they have replied
+func SelectAll(urls []string, timeout time.Duration, client *http.Client) ([]Probe, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if client == nil {
+		client = &http.Client{
+			Timeout: timeout - 100*time.Millisecond,
+		}
+	}
+
+	dst := make(chan (Probe), len(urls))
+	for _, u := range urls {
+		go func(url string) {
+			t := time.Now()
+			r, err := ctxhttp.Head(ctx, client, url)
+			if err == nil {
+				defer r.Body.Close()
+				if r.StatusCode >= 200 && r.StatusCode < 400 {
+					dst <- Probe{url, time.Since(t)}
+				}
+			}
+		}(u)
+	}
+
+	cnt := len(urls)
+	res := make([]Probe, 0, cnt)
+	for {
+		select {
+		case p := <-dst:
+			res = append(res, p)
+			cnt--
+			if cnt < 1 {
+				return res, nil
+			}
+		case <-ctx.Done():
+			return res, fmt.Errorf("Timeout")
+		}
+	}
+	return res, nil
 }
