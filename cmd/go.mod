9bc5c2381e5e53ad001f5365acfebc283a65f47d
Mikhail Anisimov <manisimov@nvidia.com>
2022-03-25 17:25:07 +0300

    small command line tool reporting SelectAll result for given URLs

---
+++ cmd/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.17
+
+require github.com/miihael/go-http-probe v0.1.0
+
+require golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+
+replace github.com/miihael/go-http-probe => ../
---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	http_probe "github.com/miihael/go-http-probe"
+)
+
+func main() {
+	fTimeout := flag.Duration("timeout", 30*time.Second, "selection timeout")
+	flag.Parse()
+	res, err := http_probe.SelectAll(flag.Args(), *fTimeout, nil)
+	fmt.Fprintf(os.Stderr, "%d responses: error=%v\n", len(res), err)
+	j := json.NewEncoder(os.Stdout)
+	j.Encode(res)
+}
